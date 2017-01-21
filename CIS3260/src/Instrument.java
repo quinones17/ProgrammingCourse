e47d0c31af9471524475181b821dd5182f349ecd
matt2 <matt2@hozonelair.attlocal.net>
2017-01-21 14:59:22 -0500

    CIS3260 Assignment 1

---
+++ CIS3260/src/Instrument.java
@@ -0,0 +1,11 @@
+
+public class Instrument {
+	String name = "Saxophone";
+	String type = "woodwind";
+	String material = "brass";
+	
+	void produceSound (){
+		System.out.println("Jazzy sound");
+	}
+
+}
---
+++ CIS3260/src/Saxophone.java
@@ -0,0 +1,11 @@
+public class Saxophone {
+
+	public static void main(String[] args) {
+		Instrument Saxophone = new Instrument();
+		Saxophone.produceSound();
+		
+		
+
+	}
+
+}
