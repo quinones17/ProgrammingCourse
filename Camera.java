b8e34c2d47f6fd878e0bba42d5e5db016ab03235
quinones17 <pquinones1@student.gsu.edu>
2017-04-30 18:57:12 -0400

    Add files via upload

---
+++ Camera.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+
+public class Camera{ 
+private Scanner scan = new Scanner(System.in);
+
+	public void camPower(String power){
+		if(power.equalsIgnoreCase("on")){
+			String[] b = {"Front Door", "Back Door", "Foyer", "Kitchen", "Garage"};
+
+			String camPower = (String) JOptionPane.showInputDialog(null, "Which camera should I turn on", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
+
+			camOn(camPower);
+			
+		}
+		else if(power.equalsIgnoreCase("off")){
+			
+			String[] b = {"Front Door", "Back Door", "Foyer", "Kitchen", "Garage"};
+
+			String camPowerOff = (String) JOptionPane.showInputDialog(null, "Which camera should I turn off", "Camera", JOptionPane.QUESTION_MESSAGE, null, b, b[1]);
+			
+	
+			camOff(camPowerOff);	
+		}
+	 
+		}
+	
+	public void camOn(String whichCam){
+	
+			System.out.println(whichCam +" is now on!" );
+		}
+	
+	public void camOff(String whichCamOff){
+		
+				System.out.println(whichCamOff+ " is now off!");
+	}
+}
+
